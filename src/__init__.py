2183a53d38d07113f8d5f240095b7b58934581de
cliffordcqf <cliffordcqf@gmail.com>
2026-08-23 09:44:18 +0800

    feat: export capital cost functions

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,22 @@
 """Financial analytics toolkit."""
 
+from .capital_cost import (
+    after_tax_cost_of_debt,
+    cost_of_equity_capm,
+    weighted_average_cost_of_capital,
+)
 from .dcf import enterprise_value, equity_value, terminal_value
 from .investment_metrics import internal_rate_of_return, net_present_value, payback_period
 
 __all__ = [
+    "after_tax_cost_of_debt",
+    "cost_of_equity_capm",
     "enterprise_value",
     "equity_value",
     "internal_rate_of_return",
     "net_present_value",
     "payback_period",
     "terminal_value",
+    "weighted_average_cost_of_capital",
 ]
 
