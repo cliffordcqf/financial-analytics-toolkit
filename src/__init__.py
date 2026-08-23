6fa0b961b42d28a066696aea38ed72f94fe3b808
cliffordcqf <cliffordcqf@gmail.com>
2026-08-23 10:36:30 +0800

    feat: export CAS classification and journal API

--- src/__init__.py
+++ src/__init__.py
@@ -13,6 +13,15 @@ from .carbon_accounting import (
     greenhouse_gas_inventory,
     scope_2_emissions,
 )
+from .cas_financial_instruments import (
+    JournalEntry,
+    JournalLine,
+    classify_financial_asset,
+    effective_interest_entry,
+    fair_value_change_entry,
+    impairment_entry,
+    initial_recognition_entry,
+)
 from .dcf import enterprise_value, equity_value, terminal_value
 from .investment_metrics import internal_rate_of_return, net_present_value, payback_period
 from .risk_metrics import (
@@ -28,18 +37,25 @@ __all__ = [
     "after_tax_cost_of_debt",
     "annualized_volatility",
     "assess_sppi",
+    "classify_financial_asset",
     "cost_of_equity_capm",
     "downside_deviation",
+    "effective_interest_entry",
     "emissions_from_activity",
     "emissions_from_sources",
     "emissions_intensity",
     "emissions_reduction",
     "enterprise_value",
     "equity_value",
+    "fair_value_change_entry",
     "greenhouse_gas_inventory",
     "historical_volatility",
+    "impairment_entry",
+    "initial_recognition_entry",
     "internal_rate_of_return",
     "log_returns",
+    "JournalEntry",
+    "JournalLine",
     "net_present_value",
     "payback_period",
     "scope_2_emissions",
