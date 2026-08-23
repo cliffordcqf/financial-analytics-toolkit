c81ac336740bec917e1f87218ecc88917464dd58
cliffordcqf <cliffordcqf@gmail.com>
2026-08-23 10:08:18 +0800

    feat: export volatility functions

--- src/__init__.py
+++ src/__init__.py
@@ -15,21 +15,33 @@ from .carbon_accounting import (
 )
 from .dcf import enterprise_value, equity_value, terminal_value
 from .investment_metrics import internal_rate_of_return, net_present_value, payback_period
+from .risk_metrics import (
+    annualized_volatility,
+    downside_deviation,
+    historical_volatility,
+    log_returns,
+    simple_returns,
+)
 
 __all__ = [
     "after_tax_cost_of_debt",
+    "annualized_volatility",
     "cost_of_equity_capm",
+    "downside_deviation",
     "emissions_from_activity",
     "emissions_from_sources",
     "emissions_intensity",
     "emissions_reduction",
     "enterprise_value",
     "equity_value",
-    "internal_rate_of_return",
     "greenhouse_gas_inventory",
+    "historical_volatility",
+    "internal_rate_of_return",
+    "log_returns",
     "net_present_value",
     "payback_period",
     "scope_2_emissions",
+    "simple_returns",
     "terminal_value",
     "weighted_average_cost_of_capital",
 ]
